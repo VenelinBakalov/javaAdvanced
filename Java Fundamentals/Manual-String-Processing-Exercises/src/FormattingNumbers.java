833f4a7e8b50ec025c511a30544c6ba409c1b004
VenelinBakalov <venelin_bakalov@abv.bg>
2017-02-01 21:57:40 +0200

    asdasd

---
+++ Java Fundamentals/Manual-String-Processing-Exercises/src/FormattingNumbers.java
@@ -0,0 +1,9 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class FormattingNumbers{
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
