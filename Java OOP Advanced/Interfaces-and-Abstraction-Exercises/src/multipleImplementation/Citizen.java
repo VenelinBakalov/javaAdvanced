bb60ab28e6acc25b51d6f4a66eae2dcf96b36646
Venelin Bakalov <venelin_bakalov@abv.bg>
2017-03-15 00:27:30 +0200

    just started Interfaces-and-Abstraction-Exercises but...can't believe I am saying this - I am too sleepy to go on and even Metallica are not able to help...well I guess this happens from time to time :D

---
+++ Java OOP Advanced/Interfaces-and-Abstraction-Exercises/src/multipleImplementation/Citizen.java
@@ -0,0 +1,39 @@
+package multipleImplementation;
+
+/**
+ * Created by Venelin on 15.3.2017 г..
+ */
+public class Citizen implements Person, Identifiable, Birthable {
+
+    private String name;
+    private int age;
+    private String id;
+    private String birthdate;
+
+    public Citizen(String name, int age, String id, String birthdate) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+
+    @Override
+    public String getBirthdate() {
+        return this.birthdate;
+    }
+
+    @Override
+    public String getId() {
+        return this.id;
+    }
+}
